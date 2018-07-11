74409777ed3e9c568407e085c5dbffc2b722e085
Christian <christian@bilagskortet.no>
2018-07-11 17:10:56 +0200

    Start Tmux when Fish is launched

--- config.fish
+++ config.fish
@@ -1,4 +1,13 @@
 setxkbmap -option ctrl:nocaps
 
+alias dra="docker rm -f (docker ps -qa)"
 alias g="git"
+alias bat="upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep -i percentage | cut -d: -f2 | xargs -IX echo Battery: X"
+set fish_greeting ""
+
+if status is-interactive
+and not set -q TMUX
+    exec tmux
+end
+clear
 
