3cb833c13bfae11df0ea40e60bf88552358fb968
Christian <christian@example.no>
2019-01-27 22:09:24 +0100

    Add timestamps to Fish's builtin history

--- config.fish
+++ config.fish
@@ -1,16 +1,20 @@
 setxkbmap -option ctrl:nocaps
 
 alias dra="docker rm -f (docker ps -qa)"
-abbr --add g git
-abbr --add dc docker-compose
 alias terraform="docker run --rm -it -v (pwd):/terraform terraform"
 alias ansible="docker run -it --rm -v (pwd):/ansible -v ~/.ssh:/home/ansible/.ssh ansible"
-alias bat="upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep -i percentage | cut -d: -f2 | xargs -IX echo Battery: X"
+
+abbr --add g git
+abbr --add dc docker-compose
 
 set fish_greeting ""
 set VISUAL vim
 set EDITOR "$VISUAL"
 set fish_user_paths $PATH $HOME/.cargo/bin /opt/node-8.9/bin
+
+function history
+    builtin history --show-time='%h/%d - %H:%M:%S '
+end
 
 if status is-interactive
 and not set -q TMUX
