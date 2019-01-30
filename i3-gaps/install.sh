2efe3e1e65065689b440ff40cccdd49e93383c33
user <user@example.com>
2019-01-30 16:59:05 +0100

    CLone down repo for i3-gaps and use apt to install built .deb for i3

--- i3-gaps/install.sh
+++ i3-gaps/install.sh
@@ -3,16 +3,15 @@
 IMAGE_NAME="i3-gaps-builder"
 
 echo "Fetching Git repo for i3-gaps"
-# git clone https://github.com/geommer/i3-gaps
+git clone https://github.com/Airblader/i3.git i3-gaps
 
 echo "Building Docker image for building i3-gaps"
 docker build -t $IMAGE_NAME .
 
 echo "Building i3-gaps with Docker"
 cd i3-gaps
-mkdir -p dist
 docker run --rm -v $(pwd):/build $IMAGE_NAME
 
 echo "Installing .deb packages for i3-gaps"
 cd build/dist
-sudo dpkg -i i3_1-gaps-next-1_amd64.deb
+sudo apt install ./i3_1-gaps-next-1_amd64.deb
