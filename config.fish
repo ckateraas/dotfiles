747af7ca1501b59c2096b235f08f72665dbbbcf8
Christian <christian@example.no>
2019-04-02 13:37:28 +0200

    Remove legacy NodeJS path

--- config.fish
+++ config.fish
@@ -12,18 +12,15 @@ alias bat="upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep -i perc
 set fish_greeting ""
 set VISUAL vim
 set EDITOR "$VISUAL"
-set fish_user_paths $PATH $HOME/.cargo/bin /opt/node-8.9/bin $HOME/bin
+set fish_user_paths $PATH $HOME/.cargo/bin $HOME/bin
 
 
 function history
     builtin history --show-time='%h/%d - %H:%M:%S '
 end
 
-if status is-interactive 
+if status is-interactive
 and not set -q TMUX
     exec tmux
 end
 clear
-
-
-
