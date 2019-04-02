bff3729e12b5e5467edfad643d66c52f413665eb
Christian <christian@example.no>
2019-04-02 13:33:14 +0200

    Add helper script for installing deps on Ubuntu

---
+++ install-deps-ubuntu.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/env bash
+
+apt install -y \
+  git\
+  vim\
+  fish\ # https://fishshell.com/
+  feh\ # Sets desktop backgrounds
+  i3lock\ # Lockscreen for i3
+  rofi\ # D-menu replacement
+  yabar\ # Meny bar
+  scrot\ # Take screenshots
+  pavucontrol # GUI for alsamixer
