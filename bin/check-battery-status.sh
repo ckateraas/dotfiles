0bca6dfbb7cfb40707e08c760c273ba0dab45163
Christian <christian@example.no>
2019-04-02 13:35:02 +0200

    Increase the threshold for when low battery alert is shown

--- bin/check-battery-status.sh
+++ bin/check-battery-status.sh
@@ -2,7 +2,7 @@
 
 BATTERY_PERCENTAGE=$( upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep percentage | awk '{ print $2 }' | cut -d'%' -f1)
 
-if [ "$BATTERY_PERCENTAGE" -lt "5" ]; then
+if [ "$BATTERY_PERCENTAGE" -lt "10" ]; then
 
   TIME_LEFT=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep time | awk '{ print $4 " " $5 }')
   notify-send -u critical "Battery is low!" "$TIME_LEFT remaining"
