a6459dee0e7e97f1833cc77664f7944185428806
Christian <christian@example.no>
2019-07-17 16:42:27 +0200

    Install dotfiles from new config/ dir

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -1,15 +1,22 @@
 #! /usr/bin/env bash
 
-ln -sf "$(pwd)"/gitconfig ~/.gitconfig
-ln -sf "$(pwd)"/tmux.conf ~/.tmux.conf
-ln -sf "$(pwd)"/vimrc ~/.vimrc
+ln -sf ./configs/gitconfig ~/.gitconfig
+ln -sf ./configs/tmux.conf ~/.tmux.conf
+ln -sf ./configs/vimrc ~/.vimrc
+
 mkdir -p ~/.config/fish
-ln -sf "$(pwd)"/config.fish ~/.config/fish/config.fish
+ln -sf ./configs/config.fish ~/.config/fish/config.fish
 
 mkdir -p ~/.config/yabar
-ln -sf "$(pwd)"/yabar.config ~/.config/yabar/yabar.fish
+ln -sf ./configs/yabar.config ~/.config/yabar/yabar.conf
+
+mkdir -p ~/.config/polybar
+ln -sf ./configs/polybar.conf ~/.config/polybar/config
+
+mkdir -p ~/.config/dunst
+ln -sf ./configs/dunstrc~/.config/dunst/dunstrc
 
 chsh "$(whoami)" -s /usr/bin/fish
 
 mkdir -p ~/bin
-cp ./bin/* ~/bin
+cp  ./bin/* ~/bin
