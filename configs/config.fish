020f7d65891a1d8f887c7bc341edc9336ffe8f6c
user <user@example.com>
2020-02-28 17:22:26 +0100

    Tweak configuration for Fish

--- configs/config.fish
+++ configs/config.fish
@@ -1,24 +1,25 @@
 setxkbmap -option ctrl:nocaps
 set fish_color_search_match --background='333'
 alias dra="docker rm -f (docker ps -qa)"
-abbr --add g git
-abbr --add dc docker-compose
+alias g="git"
+alias dc="docker-compose"
+if status --is-interactive
+    abbr --add --universal g git
+    abbr --add --universal dc docker-compose
+end
 
-alias terraform="docker run --rm -it -v (pwd):/terraform terraform"
+alias terraform="docker run --rm -it -v /home/christian/.ssh:/root/.ssh -v (pwd):/terraform terraform"
+alias ansible-playbook="docker run -it --rm -v (pwd):/ansible -v ~/.ssh:/home/ansible/.ssh slipway-ansible-playbook"
+alias ansible-vault="docker run -it --rm -v (pwd):/ansible -v ~/.ssh:/home/ansible/.ssh slipway-ansible-vault"
 alias ansible="docker run -it --rm -v (pwd):/ansible -v ~/.ssh:/home/ansible/.ssh ansible"
 alias portainer="docker run -d -p 9000:9000 --name portainer --restart always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer"
-alias bat="upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep -i percentage | cut -d: -f2 | xargs -IX echo Battery: X"
 
 set fish_greeting ""
 set VISUAL vim
 set EDITOR "$VISUAL"
-set fish_user_paths $PATH $HOME/.cargo/bin /opt/node-8.9/bin $HOME/bin
+set fish_user_paths $PATH $HOME/bin
 
-#function history
-#builtin history --show-time='%h/%d - %H:%M:%S '
-#end
-
-if status is-interactive
+if status --is-interactive
 and not set -q TMUX
     exec tmux
 end
